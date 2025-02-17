ac0ded1244f868ad78916f839ef0602ecaad9f7f
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-02-17 20:40:56 +0200

    Fitst commit

---
+++ src/test/kotlin/com/dopc/DopcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dopc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DopcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
