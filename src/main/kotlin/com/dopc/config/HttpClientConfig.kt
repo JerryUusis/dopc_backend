34089c119a1a065dbb9306043f77ad3deb485e10
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-02-18 22:56:37 +0200

    Isolate httpClient as a bean. Install Mockk, springmockk and assertJ. Add unit test for getVenueData method.

---
+++ src/main/kotlin/com/dopc/config/HttpClientConfig.kt
@@ -0,0 +1,13 @@
+package com.dopc.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.net.http.HttpClient
+
+@Configuration
+class HttpClientConfig {
+    @Bean
+    fun httpClient(): HttpClient {
+        return HttpClient.newBuilder().build()
+    }
+}
